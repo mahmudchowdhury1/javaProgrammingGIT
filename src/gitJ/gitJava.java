aa0b7ada3b88095257570c7754b110464549bcc3
mahmud <mahmudhc1@gmail.com>
2025-11-23 23:28:54 -0500

    P1

--- src/gitJ/gitJava.java
+++ src/gitJ/gitJava.java
@@ -9,6 +9,10 @@ public class gitJava {
 		System.out.println("My name is mahmud1");
 		System.out.println("My name is mahmud2");
 		System.out.println("My name is mahmud4");
+		System.out.println("My name is mahmud5");
+		System.out.println("My name is mahmud6");
+		System.out.println("My name is mahmud7");
+	}
 	}
 
 }
